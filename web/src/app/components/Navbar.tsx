297171034ada8a7140f8409a46eebc89eb10f006
nadiraaraa <nadiraaraa@gmail.com>
2024-06-09 15:28:14 +0700

    Update Navbar.tsx

--- web/src/app/components/Navbar.tsx
+++ web/src/app/components/Navbar.tsx
@@ -27,7 +27,7 @@ const Navbar: React.FC<NavProps> = ({ currPage }) => {
 
   return (
     <div className="fixed w-full bg-blue1 h-[4.5rem] p-4 flex items-center justify-between">
-      <button onClick={() => (window.location.href = "/")} className={(currPage === "dashboard" ? "text-blue2 hover:text-black1 " : "text-white ") + "font-bold hhover:bg-pink1 ml-4  py-2 rounded-sm"}>
+      <button onClick={() => (window.location.href = "/dashboard")} className={(currPage === "dashboard" ? "text-blue2 hover:text-black1 " : "text-white ") + "font-bold hhover:bg-pink1 ml-4  py-2 rounded-sm"}>
         <img src="/logo.png" alt="logo" className="w-52"></img>
       </button>
       {/* <button onClick={() => (window.location.href = "/dashboard")} className= {(currPage ==="Transcript" ? "text-blue2 hover:text-black1 " : "text-white ")  + "font-bold hover:bg-pink1 pt-2 px-6 rounded-sm"}>Transcript</button> */}
